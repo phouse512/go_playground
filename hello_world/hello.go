7ea610584ba336ba11edb1c54dc5717a414701db
phouse512 <philiphouse2015@u.northwestern.edu>
2016-01-14 23:26:51 -0600

    adding stringutil

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/phouse512/go_playground/stringutil"
+)
 
 func main() {
   fmt.Printf("hello, world!!")
+  fmt.Printf(stringutil.Reverse("ybab og\n"))
 }
 
